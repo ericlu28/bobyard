60d739a4694a4f8733698f2fccba1e5d6f443979
Eric Lu <eric.andrew.lu@gmail.com>
2026-06-20 10:18:25 -0700

    DB interface

---
+++ comments/src/main/java/com/bobyard/comments/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.bobyard.comments.repository;
+
+import com.bobyard.comments.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
